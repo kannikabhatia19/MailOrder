c8af4466fa9dec4c20707022553c3506f5831bb7
Kannika Bhatia <200332992@student.georgianc.on.ca>
2017-01-23 13:10:34 -0500

    Delete unuse images

--- MailOrder/SalesBonus.Designer.cs
+++ MailOrder/SalesBonus.Designer.cs
@@ -208,8 +208,7 @@
             // 
             // SalesBonus
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.ClientSize = new System.Drawing.Size(438, 436);
             this.Controls.Add(this.PrintButton);
             this.Controls.Add(this.NextButton);
